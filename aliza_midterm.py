cda0baa1a0e657e90084bd2208306b8d7d1c90ad
Aliza-i123 <157236853+Aliza-i123@users.noreply.github.com>
2024-01-21 07:17:07 +0000

    Add files via upload

---
+++ aliza_midterm.py
@@ -0,0 +1,66 @@
+#  Aliza
+
+#   Queation No 1
+#   Add two variable and print 
+
+
+num1 = int(input("Enter 1st number : "))
+num2 = int(input("Enter 2nd number : "))
+sum = num1 + num2
+print("the sum of ",num1,"+",num2,"is",sum)
+
+#  Calculating Division
+num1 = int(input("Enter 1st number : "))
+num2 = int(input("Enter 2nd number : "))
+division = num1 / num2
+print("division : ", num1, "/", num2, "is", division)
+
+#  Calculating Power
+power = num1 ** num2
+print("power : ",num1,"^",num2,"is",power)
+
+# Calculating Floor Division
+num1 = int(input("Enter 1st number : "))
+num2 = int(input("Enter 2nd number : "))
+floor = num1 // num2
+print("floor :",num1, "//",num2, "is", floor)
+
+# Question No 2
+# Area of triangle
+length = float(input("Enter the lenght base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+# Calculating the Area of triangle
+area = 0.5* length * height
+print(f"the area of triangle is: ",area)
+
+# Question No 3
+# Convert Celsius to Fahrenheit
+celsius = float(input("Enter the temperature in Celsius :"))
+fahrenheit = (celsius * 9/5)+32
+print(f"Celsius to pahrenheit is :", fahrenheit)
+
+# Question No 4
+# Data types
+# integer
+num1 = 10
+print("the data type of", num1 ,"is",type(num1))
+
+float_number = 10.5
+print("the data type of", float_number, "is",type(float_number))
+
+string = "Hello World"
+print("the data type of", string,"is",type(string))
+
+boolen = True
+print("the data type of", boolen ,"is",type(boolen))
+
+lst = [1,2,3,4,5]
+print("the data type of", lst, "is",type(lst))
+
+tup = (1,2,3,4,5)
+print("the data type of", tup ,"is",type(tup))
+
+sett = {1,2,3,4,5}
+print("the data type of", sett, "is",type(sett))
+
+
